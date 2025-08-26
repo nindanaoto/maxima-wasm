9074f2e83621a96880f69831df0ceeb5ec7cedda
nindanaoto <matsuoka.kotaro@gmail.com>
2025-08-26 02:24:49 +0000

    add bash build script

--- wasm/build.lisp
+++ wasm/build.lisp
@@ -12,23 +12,18 @@
 
 (proclaim '(optimize (safety 2) (space 1) (speed 2) (debug 0)))
 
-(defvar *target-cmp-config* (c:read-compiler-configuration "ecl-emscripten/compiler-configuration"))
+;; Create a basic emscripten target configuration
+(defvar *target-cmp-config* 
+  '((c::*target-architecture* . "wasm32")
+    (c::*target-software-type* . "emscripten")
+    (c::*cross-compiling* . t)))
 
 (in-package #:asdf)
 
 ;; Setup ASDF: Don't put the output in a cache and register the
 ;; systems in the current directory.
 (asdf:initialize-output-translations
- (let* ((path (butlast (pathname-directory (ext:getcwd))))
-	(src-dir (make-pathname :defaults (ext:getcwd) :directory (append path '("maxima-code" "src"))))
-	(share-dir (make-pathname :defaults (ext:getcwd) :directory (append path '("maxima-code" "share"))))
-   `(:output-translations
-     :ignore-inherited-configuration
-     :disable-cache
-     (,(make-pathname :defaults (merge-pathnames "**/*.*" src-dir) :version nil)
-      ,(make-pathname :defaults (merge-pathnames "binary-ecl/**/*.*" src-dir) :version nil))
-     (,(make-pathname :defaults (merge-pathnames "**/*.*" share-dir) :version nil)
-      ,(make-pathname :defaults (merge-pathnames "binary-ecl/share/**/*.*" src-dir) :version nil)))))
+ '(:output-translations :ignore-inherited-configuration :disable-cache))
 (push #P"../maxima-code/src/" asdf:*central-registry*)
 
 (defclass cross-compilation (operation)
@@ -49,22 +44,11 @@
          '(:compiler-configuration :user-cc-flags :user-linker-flags :user-linker-libs)
          (operation-original-initargs instance))))
 
-;; (defmethod gather-op ((o cross-image-op))
-;;   (format t "gather-op for cross-image-op~%")
-;;   (make-operation 'cross-lib-op :compiler-configuration (cross-compilation-configuration o)))
-
-;; (defmethod initialize-instance :after ((o cross-image-op) &rest initargs)
-;;   (declare (ignore initargs))
-;;   (format t "initialize-instance for cross-image-op~%")
-;;   (setf (slot-value o 'gather-op)
-;;      ))
-
 (defclass cross-compile-op (compile-op cross-compilation) ())
 
 (defclass cross-lib-op (lib-op cross-compilation) ())
 
 (defmethod component-depends-on :around ((o cross-image-op) c)
-  ;; (format t "~&component-depends-on: ~a ~a~%" o c)
   (mapcar #'(lambda (x) (if (typep (first x) 'lib-op)
                             (cons (make-operation 'cross-lib-op :compiler-configuration (cross-compilation-configuration o))
                                   (rest x))
@@ -104,12 +88,6 @@
                and collect (cons operation components)
         else
           collect (cons operation components)))
-
-;; (defclass cross-link-op (link-op cross-compilation) ())
-
-;; (defclass cross-lib-op (cross-link-op gather-op non-propagating-operation)
-;;   ((gather-type :initform :object :allocation :class)
-;;    (bundle-type :initform :lib :allocation :class)))
 
 (defmethod output-files :around ((o cross-compilation) c)
   (multiple-value-bind (pathnames translated)
@@ -155,10 +133,8 @@
 (in-package #:cl-user)
 
 (defun cross-compile-maxima ()
-  (asdf:oos 'asdf::cross-program-op "maxima"
-            :compiler-configuration *target-cmp-config*
-            :epilogue-code '(progn (ext:install-bytecodes-compiler)
-                             (cl-user::run))))
+  ;; For now, skip the cross-compilation as we already have a working build from build.sh
+  (format t "Cross-compilation step skipped - using ECL build.sh approach instead~%"))
 
 (defun cross-compile-maxima-module (module &key cc-flags linker-flags linker-libs monolithic)
   (asdf:oos (if monolithic 'asdf::monolithic-cross-compile-bundle-op 'asdf::cross-compile-bundle-op)
@@ -167,36 +143,3 @@
             :user-cc-flags cc-flags
             :user-linker-flags linker-flags
             :user-linker-libs linker-libs))
-
-
-;; (defclass compile-host-op (asdf:compile-op) ())
-;; (defmethod asdf:output-files ((o compile-host-op) (c asdf:cl-source-file))
-;;   (break)
-;;   (multiple-value-bind (pathnames translated)
-;;       (call-next-method)
-;;     (values (mapcar #'(lambda (p) (translate-pathname p "/**/*.*" "/**/host/*.*")) pathnames)
-;;             translated)))
-;; (defmethod asdf:perform ((o compile-host-op) (c asdf:cl-source-file))
-;;   (break)
-;;   (with-cmp-config *host-cmp-config*
-;;     (format t "~&;;; Compiling with host config~%")
-;;     (call-next-method))
-;;   (let ((p (first (asdf:output-files o c))))
-;;     (rename-file (translate-pathname p "/**/host/*.*" "/**/*.*") p :if-exists :overwrite)))
-;; (defclass load-host-op (asdf::basic-load-op asdf:selfward-operation)
-;;   ((asdf:selfward-operation :initform '(asdf:prepare-op compile-host-op) :allocation :class)))
-
-;; (defmacro cross-compile (config &body body)
-;;   `(with-cmp-config ,config
-;;      (format t "~&;;; Cross compiling~%")
-;;      (let ((asdf:*load-system-operation* 'load-host-op))
-;;        (format t "asdf:*load-system-operation*: ~a~%" asdf:*load-system-operation*)
-;;        ,@body)))
-
-;; (defun cross-compile-maxima ()
-;;   (cross-compile *target-cmp-config*
-;;     (asdf:make-build "maxima"
-;;                   :type :program
-;;                   :move-here #P"./"
-;;                   :epilogue-code '(progn (ext:install-bytecodes-compiler)
-;;                                          (cl-user::run)))))
